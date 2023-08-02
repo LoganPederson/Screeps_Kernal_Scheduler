6e644e7287f2e8338e5215288b1cb37acb0ee671
SinlessMirror <loganpederson@gmail.com>
2023-08-02 17:54:07 -0500

    refactored for readability

--- builderProcess.js
+++ builderProcess.js
@@ -14,7 +14,6 @@ class BuilderProcess extends Process {
         // Variables + Upgrade Info
         const room = Game.rooms[this.data.roomName];
         const spawn = room.find(FIND_MY_SPAWNS)[0]
-        const constructionSites = room.find(FIND_CONSTRUCTION_SITES)
         if(!Game.creeps[this.data.creepName]){
             this.finished = true;
             return
@@ -22,36 +21,43 @@ class BuilderProcess extends Process {
         // Define creep once we know it's alive
         const creep = Game.creeps[this.data.creepName];
 
-        const priority1 = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES, {
-            filter: function(object){
-                return object.structureType === 'container'
-            }
-        });
-        const priority2 = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES, {
-            filter: function(object){
-                return object.structureType === 'extension'
-            }
-        });
-        const priority3 = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-
-        const target = ()=>{
-            if(priority1.length > 0){
-                return creep.pos.findClosestByPath(priority1)
-            }
-            else if(priority2.length > 0){  
-                return creep.pos.findClosestByPath(priority2)
-            }
-            else if(priority3.length > 0){  
-                return creep.pos.findClosestByPath(priority3)
-            }
-        }
-        
         // Initialize Process into Memory
         if(!creep.memory.process){
             creep.memory.process = this.pid
         }
+
+
+        // 
+        // Set construction_site priority by structureType property
+        //
+        const priority1 = room.find(FIND_CONSTRUCTION_SITES, {
+            filter: function(object){
+                return object.structureType === 'container'
+            }
+        });
+        const priority2 = room.find(FIND_CONSTRUCTION_SITES, {
+            filter: function(object){
+                return object.structureType === 'extension'
+            }
+        });
+        const priority3 = room.find(FIND_CONSTRUCTION_SITES);
+
+        const determineTarget = function(){
+            if(priority1 && priority1.length > 0){
+                return creep.pos.findClosestByPath(priority1)
+            }
+            else if(priority2 && priority2.length > 0){  
+                return creep.pos.findClosestByPath(priority2)
+            }
+            else if(priority3 && priority3.length > 0){  
+                return creep.pos.findClosestByPath(priority3)
+            }
+        }
+        const target = determineTarget()
+
         // If energy, build
         if(target) {
+            console.log(target)
             if(creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
                 if(creep.build(target) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
